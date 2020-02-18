0cc31a9470db3a76745dfb84fcf67c109ee45bbb
Juan Ibiapina <juanibiapina@gmail.com>
2020-02-18 23:42:33 +0100

    feat: Change it into a simple key value store

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 #![feature(with_options)]
 
 use structopt::StructOpt;
-use failure::{Error, ResultExt};
+use failure::Error;
 
-use std::fs;
+use std::collections::HashMap;
 use std::fs::File;
-use std::io::Read;
-use std::io::Write;
 use std::path::PathBuf;
+use std::process;
 
 #[derive(StructOpt, Debug)]
 struct Args {
@@ -20,42 +19,65 @@ struct Args {
 #[derive(StructOpt, Debug)]
 enum Command {
     Add {
-        entries: Vec<String>,
+        key: String,
+        value: String,
     },
-    List,
+    Get {
+        key: String,
+    },
 }
 
 fn config_file_path(args: &Args) -> Result<PathBuf, Error> {
     match args.config_file {
         Some(ref path) => {
-            Ok(fs::canonicalize(path)?)
+            Ok(PathBuf::from(path))
         },
         None => {
             let xdg_dirs = xdg::BaseDirectories::with_prefix("shelf")?;
 
-            Ok(xdg_dirs.place_config_file("shelf.txt")?)
+            Ok(xdg_dirs.place_config_file("shelf.yml")?)
         },
     }
+}
+
+fn read_config(args: &Args) -> Result<HashMap<String, String>, Error> {
+    let config_path = config_file_path(args)?;
+
+    match File::open(config_path) {
+        Ok(file) => Ok(serde_yaml::from_reader(file)?),
+        Err(_) => Ok(HashMap::new()),
+    }
+}
+
+fn write_config(args: &Args, data: &HashMap<String, String>) -> Result<(), Error> {
+    let config_path = config_file_path(args)?;
+
+    let file = File::create(config_path)?;
+
+    serde_yaml::to_writer(file, data)?;
+
+    Ok(())
 }
 
 fn actual_main() -> Result<(), Error> {
     let args = Args::from_args();
 
     match args.cmd {
-        Command::Add { ref entries } => {
-            let config_path = config_file_path(&args)?;
-            let mut file = File::with_options().create(true).append(true).open(config_path).context("Couldn't create config file")?;
-            writeln!(&mut file, "{}", entries.join(","))?;
+        Command::Add { ref key, ref value } => {
+            let mut data = read_config(&args)?;
+
+            data.insert(key.to_owned(), value.to_owned());
+
+            write_config(&args, &data)?;
 
         },
-        Command::List => {
-            let config_path = config_file_path(&args)?;
-            let mut file = File::open(config_path).context("Couldn't read config file")?;
+        Command::Get { ref key } => {
+            let data = read_config(&args)?;
 
-            let mut buffer = String::new();
-            file.read_to_string(&mut buffer)?;
-
-            print!("{}", buffer);
+            match data.get(key) {
+                Some(value) => println!("{}", value),
+                None => {},
+            }
         }
     }
 
@@ -65,6 +87,9 @@ fn actual_main() -> Result<(), Error> {
 fn main() {
     match actual_main() {
         Ok(()) => {},
-        Err(err) => println!("{}", err),
+        Err(err) => {
+            println!("{}", err);
+            process::exit(1);
+        },
     }
 }
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -2,17 +2,15 @@ use assert_cmd::Command;
 use assert_fs::prelude::*;
 
 #[test]
-fn add_and_list_items() {
+fn add_and_get_items() {
     let temp = assert_fs::TempDir::new().unwrap();
-    let config_file = temp.child("foo.txt");
-    config_file.touch().unwrap();
+    let config_file = temp.child("foo.yml");
 
     Command::cargo_bin("shelf").unwrap()
         .arg("-c")
         .arg(config_file.path())
         .arg("add")
-        .arg("level 1")
-        .arg("level 2")
+        .arg("key")
         .arg("value")
         .assert()
         .success();
@@ -22,15 +20,25 @@ fn add_and_list_items() {
         .arg(config_file.path())
         .arg("add")
         .arg("entry 2")
-        .arg("false")
+        .arg("word")
         .assert()
         .success();
 
     Command::cargo_bin("shelf").unwrap()
         .arg("-c")
         .arg(config_file.path())
-        .arg("list")
+        .arg("get")
+        .arg("key")
         .assert()
         .success()
-        .stdout("level 1,level 2,value\nentry 2,false\n");
+        .stdout("value\n");
+
+    Command::cargo_bin("shelf").unwrap()
+        .arg("-c")
+        .arg(config_file.path())
+        .arg("get")
+        .arg("entry 2")
+        .assert()
+        .success()
+        .stdout("word\n");
 }

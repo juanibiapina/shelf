0446936f77ad578c609645ce9132c5de760a1903
Juan Ibiapina <juanibiapina@gmail.com>
2020-02-18 21:56:25 +0100

    feat: Add `add` and `list` commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,70 @@
+#![feature(with_options)]
+
+use structopt::StructOpt;
+use failure::{Error, ResultExt};
+
+use std::fs;
+use std::fs::File;
+use std::io::Read;
+use std::io::Write;
+use std::path::PathBuf;
+
+#[derive(StructOpt, Debug)]
+struct Args {
+    #[structopt(short, long, env = "SHELF_CONFIG")]
+    config_file: Option<String>,
+    #[structopt(subcommand)]
+    cmd: Command,
+}
+
+#[derive(StructOpt, Debug)]
+enum Command {
+    Add {
+        message: Vec<String>,
+    },
+    List,
+}
+
+fn config_file_path(args: &Args) -> Result<PathBuf, Error> {
+    match args.config_file {
+        Some(ref path) => {
+            Ok(fs::canonicalize(path)?)
+        },
+        None => {
+            let xdg_dirs = xdg::BaseDirectories::with_prefix("shelf")?;
+
+            Ok(xdg_dirs.place_config_file("shelf.txt")?)
+        },
+    }
+}
+
+fn actual_main() -> Result<(), Error> {
+    let args = Args::from_args();
+
+    match args.cmd {
+        Command::Add { ref message } => {
+            let config_path = config_file_path(&args)?;
+            let mut file = File::with_options().create(true).append(true).open(config_path).context("Couldn't create config file")?;
+            writeln!(&mut file, "{}", message.join(" "))?;
+
+        },
+        Command::List => {
+            let config_path = config_file_path(&args)?;
+            let mut file = File::open(config_path).context("Couldn't read config file")?;
+
+            let mut buffer = String::new();
+            file.read_to_string(&mut buffer)?;
+
+            print!("{}", buffer);
+        }
+    }
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    match actual_main() {
+        Ok(()) => {},
+        Err(err) => println!("{}", err),
+    }
 }
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,8 +1,34 @@
 use assert_cmd::Command;
+use assert_fs::prelude::*;
 
 #[test]
-fn success() {
-    let mut cmd = Command::cargo_bin("shelf").unwrap();
+fn add_and_list_items() {
+    let temp = assert_fs::TempDir::new().unwrap();
+    let config_file = temp.child("foo.txt");
+    config_file.touch().unwrap();
 
-    cmd.assert().success();
+    Command::cargo_bin("shelf").unwrap()
+        .arg("-c")
+        .arg(config_file.path())
+        .arg("add")
+        .arg("entry")
+        .arg("1")
+        .assert()
+        .success();
+
+    Command::cargo_bin("shelf").unwrap()
+        .arg("-c")
+        .arg(config_file.path())
+        .arg("add")
+        .arg("entry 2")
+        .assert()
+        .success();
+
+    Command::cargo_bin("shelf").unwrap()
+        .arg("-c")
+        .arg(config_file.path())
+        .arg("list")
+        .assert()
+        .success()
+        .stdout("entry 1\nentry 2\n");
 }

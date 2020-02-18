20f99bd28aa76e54a34509fd8a11c022e3bf53f4
Juan Ibiapina <juanibiapina@gmail.com>
2020-02-18 21:08:34 +0100

    feat: Rename project to shelf

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -2,7 +2,7 @@ use assert_cmd::Command;
 
 #[test]
 fn success() {
-    let mut cmd = Command::cargo_bin("stuff").unwrap();
+    let mut cmd = Command::cargo_bin("shelf").unwrap();
 
     cmd.assert().success();
 }
